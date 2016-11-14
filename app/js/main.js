7ac0fd93778772060364a99a4022496ad868def6
Suong Kevin Tan <suongkevintan@MacBook-Pro-de-Suong.local>
2016-11-14 14:02:26 +0100

    [front-end:home-page] Improving the Home Page
    Enhancing animation of the menu

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,9 @@ menu__button.addEventListener('click',() =>{
   menu__button.querySelector('.menu__button-lineFirst').classList.toggle('active')
   menu__button.querySelector('.menu__button-lineSecond').classList.toggle('active')
   document.querySelector('.revealMenu').classList.toggle('active')
+  document.querySelector('.overlayScreen').classList.toggle('active')
+  console.log("prout");
+  
 })
 
 var links = document.querySelectorAll('.revealMenu__list--link');
