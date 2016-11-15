121722d549b9cda6edcd46f8118444058d2df4b8
Adrien Zaganelli <adrienzaganelli@gmail.com>
2016-11-16 00:29:40 +0100

    :construction:  [story:scroll] quick & dirty hack scroll

--- app/js/main.js
+++ app/js/main.js
@@ -32,11 +32,86 @@ function launch() {
             document.querySelector(".revealMenu__imageContainer--picture").setAttribute("src", sourcePicture);
         });
     });
-    
-    document.querySelector("#container_story").addEventListener('mousewheel', (e) => {
-        document.querySelector("#container_story").scrollLeft -= (e.deltaX * 20);
-        document.querySelector("#container_story").scrollRight -= (e.deltaX * 20);
-        document.querySelector("#container_story").scrollTop = 0;
+
+    // ('mousewheel', (e) => {
+    //     document.querySelector("#container_story").scrollLeft -= (e.deltaX * 20);
+    //     document.querySelector("#container_story").scrollRight -= (e.deltaX * 20);
+    //     document.querySelector("#container_story").scrollTop = 0;
+    // });
+    // using on
+    document.querySelector("#container_story").addEventListener('mousewheel', function(event){
+    let orgEvent = event || window.event
+    let args = [].slice.call( arguments, 1 )
+    let delta = 0
+    let returnValue = true
+    let deltaX = 0
+    let deltaY = 0;
+    event = (orgEvent);
+    //event.type  = "mousewheel";
+
+    // Old school scrollwheel delta
+    if ( orgEvent.wheelDelta ) { delta = orgEvent.wheelDelta/120; }
+    if ( orgEvent.detail     ) { delta = -orgEvent.detail/3; }
+
+    // New school multidimensional scroll (touchpads) deltas
+    deltaY = delta;
+
+    // Gecko
+    if ( orgEvent.axis !== undefined && orgEvent.axis === orgEvent.HORIZONTAL_AXIS ) {
+        deltaY = 0;
+        deltaX = -1*delta;
+    }
+
+    // Webkit
+    if ( orgEvent.wheelDeltaY !== undefined ) { deltaY = orgEvent.wheelDeltaY/120; }
+    if ( orgEvent.wheelDeltaX !== undefined ) { deltaX = -1*orgEvent.wheelDeltaX/120; }
+
+    // Add event and delta to the front of the arguments
+    console.log(orgEvent)
+    args.unshift(event, delta, deltaX, deltaY);
+    event.preventDefault();
+
+         document.querySelector("#container_story").scrollLeft -= (delta * 30);
+
+        console.log(event.deltaX, event.deltaY, event.deltaFactor);
+
+    });
+    document.querySelector("#container_story").addEventListener('DOMMouseScroll', function(event){
+    let orgEvent = event || window.event
+    let args = [].slice.call( arguments, 1 )
+    let delta = 0
+    let returnValue = true
+    let deltaX = 0
+    let deltaY = 0;
+    event = (orgEvent);
+    //event.type  = "mousewheel";
+
+    // Old school scrollwheel delta
+    if ( orgEvent.wheelDelta ) { delta = orgEvent.wheelDelta/120; }
+    if ( orgEvent.detail     ) { delta = -orgEvent.detail/3; }
+
+    // New school multidimensional scroll (touchpads) deltas
+    deltaY = delta;
+
+    // Gecko
+    if ( orgEvent.axis !== undefined && orgEvent.axis === orgEvent.HORIZONTAL_AXIS ) {
+        deltaY = 0;
+        deltaX = -1*delta;
+    }
+
+    // Webkit
+    if ( orgEvent.wheelDeltaY !== undefined ) { deltaY = orgEvent.wheelDeltaY/120; }
+    if ( orgEvent.wheelDeltaX !== undefined ) { deltaX = -1*orgEvent.wheelDeltaX/120; }
+
+    // Add event and delta to the front of the arguments
+    console.log(orgEvent)
+    args.unshift(event, delta, deltaX, deltaY);
+    event.preventDefault();
+
+         document.querySelector("#container_story").scrollLeft -= (delta * 30);
+
+        console.log(event.deltaX, event.deltaY, event.deltaFactor);
+
     });
 
 }
