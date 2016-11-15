14c5df7d7f5794159b54acf3580293d0c2d4660d
CMarzin <corentinmarzin@gmail.com>
2016-11-15 19:07:59 +0100

    [story|menu] add menu to story

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,24 @@ Loader.loadData('data/data.json', function(data) {
 
         document.querySelector('#story').innerHTML = (story);
 
+        var menu__button = document.querySelector('.menu__button');
+menu__button.addEventListener('click',() =>{
+  menu__button.querySelector('.menu__button-lineFirst').classList.toggle('active')
+  menu__button.querySelector('.menu__button-lineSecond').classList.toggle('active')
+  document.querySelector('.revealMenu').classList.toggle('active')
+  document.querySelector('.overlayScreen').classList.toggle('active')
+  console.log("prout");
+
+})
+
+var links = document.querySelectorAll('.revealMenu__list--link');
+[].forEach.call(links, (elem)=>{
+  elem.addEventListener('mouseenter', function(e) {
+    var sourcePicture = elem.getAttribute('data-src');
+    document.querySelector(".revealMenu__imageContainer--picture").setAttribute("src",sourcePicture);
+  });
+});
+
     }
 
     launch();
