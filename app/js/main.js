03e635471dd171c73d2baea08683bab3c2fa4b95
Adrien Zaganelli <adrienzaganelli@gmail.com>
2016-11-15 23:20:20 +0100

    [story:js] hack scroll

--- app/js/main.js
+++ app/js/main.js
@@ -5,14 +5,12 @@
 import {loadData} from './loader-data.js'
 
 let data = new loadData('data/data.json', () => {
-        data = data.responseText;
-        data = JSON.parse(data);
-        const story = MyApp.templates.story(data);
-        document.querySelector('#story').innerHTML = (story);
-        launch();
-    }
-
-);
+    data = data.responseText;
+    data = JSON.parse(data);
+    const story = MyApp.templates.story(data);
+    document.querySelector('#story').innerHTML = (story);
+    launch();
+});
 
 
 function launch() {
@@ -33,6 +31,12 @@ function launch() {
             var sourcePicture = elem.getAttribute('data-src');
             document.querySelector(".revealMenu__imageContainer--picture").setAttribute("src", sourcePicture);
         });
+    });
+    
+    document.querySelector("#container_story").addEventListener('mousewheel', (e) => {
+        document.querySelector("#container_story").scrollLeft -= (e.deltaX * 20);
+        document.querySelector("#container_story").scrollRight -= (e.deltaX * 20);
+        document.querySelector("#container_story").scrollTop = 0;
     });
 
 }
