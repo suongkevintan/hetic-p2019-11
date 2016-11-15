7d40ce1de5ead51cb006ef1410343548d4e00fe9
CMarzin <corentinmarzin@gmail.com>
2016-11-15 22:10:03 +0100

    [ajax|es6] convert call ajax es5 in call es6 and update webpack

--- app/js/loader-data.js
+++ app/js/loader-data.js
@@ -1,20 +1,16 @@
-const Loader = function( exports )
-{
-    exports.loadData = function(json,cb){
+export class loadData {
+    constructor(json, cb) {
 
         const xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
         xobj.open('GET', json, true);
-        xobj.onreadystatechange = function () {
+        xobj.onreadystatechange = function() {
 
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            if(cb)cb(xobj.responseText);
-          }
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                if (cb)cb(xobj.responseText);
+            }
         };
-
-        xobj.send(null);
+      xobj.send(null);
+      return xobj;
     }
-
-    return exports;
-
-}({});
+}
--- app/js/main.js
+++ app/js/main.js
@@ -2,35 +2,39 @@
 // ! take json data and display them  //
 // ==================================//
 
-Loader.loadData('data/data.json', function(data) {
-    data = JSON.parse(data);
-    const s = data;
+import {loadData} from './loader-data.js'
 
-    function launch() {
-
-        const story = MyApp.templates.story(s);
-
+let data = new loadData('data/data.json', () => {
+        data = data.responseText;
+        data = JSON.parse(data);
+        const story = MyApp.templates.story(data);
         document.querySelector('#story').innerHTML = (story);
-
-        var menu__button = document.querySelector('.menu__button');
-menu__button.addEventListener('click',() =>{
-  menu__button.querySelector('.menu__button-lineFirst').classList.toggle('active')
-  menu__button.querySelector('.menu__button-lineSecond').classList.toggle('active')
-  document.querySelector('.revealMenu').classList.toggle('active')
-  document.querySelector('.overlayScreen').classList.toggle('active')
-  console.log("prout");
-
-})
-
-var links = document.querySelectorAll('.revealMenu__list--link');
-[].forEach.call(links, (elem)=>{
-  elem.addEventListener('mouseenter', function(e) {
-    var sourcePicture = elem.getAttribute('data-src');
-    document.querySelector(".revealMenu__imageContainer--picture").setAttribute("src",sourcePicture);
-  });
-});
-
+        launch();
     }
 
-    launch();
-});
+);
+
+
+function launch() {
+
+    var menu__button = document.querySelector('.menu__button');
+    menu__button.addEventListener('click', () => {
+        menu__button.querySelector('.menu__button-lineFirst').classList.toggle('active')
+        menu__button.querySelector('.menu__button-lineSecond').classList.toggle('active')
+        document.querySelector('.revealMenu').classList.toggle('active')
+        document.querySelector('.overlayScreen').classList.toggle('active')
+        console.log("prout");
+
+    })
+
+    var links = document.querySelectorAll('.revealMenu__list--link');
+    [].forEach.call(links, (elem) => {
+        elem.addEventListener('mouseenter', function(e) {
+            var sourcePicture = elem.getAttribute('data-src');
+            document.querySelector(".revealMenu__imageContainer--picture").setAttribute("src", sourcePicture);
+        });
+    });
+
+}
+
+// launch();
