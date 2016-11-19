52dc7c7060e6bfb12ef684dfd67f839fd110cd88
CMarzin <corentinmarzin@gmail.com>
2016-11-19 18:09:21 +0100

    [dev|all] updtage gulpfile/webpack menu is now working add handlebar join 3 page home/story/credit

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,7 @@
-// ===================================//
-// ! take json data and display them  //
-// ==================================//
-
 import {loadData} from './loader-data.js'
 import {scrollHorizontal} from './models/class.scroll-horizontal.js'
+import {AnimationInterface} from './models/class.animation.js'
+import {Particule} from './models/class.particule.js'
 import {DomManipulator} from './models/class.dommanipulator.js'
 
 // import {Fidget} from './fidget.js'
@@ -11,43 +9,45 @@ import {DomManipulator} from './models/class.dommanipulator.js'
 // window.$ = new DomManipulator();
 // window.Fidget = new Fidget();
 
+// replace some Jquery function with vanilla Javascript
 let $ = new DomManipulator();
+// Particule js
+let particule = new Particule();
+// All animation site
+let animation = new AnimationInterface();
+
+// ===================================//
+// ! take json data and display them  //
+// ==================================//
 
 let data = new loadData('data/data.json', () => {
+    // Load data from data.json
     data = data.responseText;
+    // Parse data
     data = JSON.parse(data);
-    const story = MyApp.templates.story(data);
+    // throw data in the file home/story/contact in the folder templates
+    const home    = MyApp.templates.home(data);
+    const story   = MyApp.templates.story(data);
+    const contact = MyApp.templates.contact(data);
+    // Insert information in DOM
+    $.el('#home').innerHTML = (home);
     $.el('#story').innerHTML = (story);
-    launchStoryMode();
+    $.el('#contact').innerHTML = (contact);
+    // launchScrollConverter();
 });
 
-function launchStoryMode() {
-
-
-    //menu animtion
-    let menu__button = $.el('.menu__button');
-    menu__button.addEventListener('click', () => {
-        menu__button.querySelector('.menu__button-lineFirst').classList.toggle('active')
-        menu__button.querySelector('.menu__button-lineSecond').classList.toggle('active')
-        $.el('.revealMenu').classList.toggle('active')
-        $.el('.overlayScreen').classList.toggle('active')
-    })
-
-    //menu item hover
-    let links = $.all('.revealMenu__list--link');
-    [].forEach.call(links, (elem) => {
-        elem.addEventListener('mouseenter', function(e) {
-            let sourcePicture = elem.getAttribute('data-src');
-            $.el(".revealMenu__imageContainer--picture").setAttribute("src", sourcePicture);
-        });
-    });
-
-    //hack vertical scroll to horizontal scroll
-
-    let containerInvertScroll = $.el("#container_story");
-    //Webkit
-    containerInvertScroll.addEventListener('mousewheel', () => new scrollHorizontal(window.event, containerInvertScroll))
-    //Gecko
-    containerInvertScroll.addEventListener('DOMMouseScroll', () => new scrollHorizontal(window.event, containerInvertScroll))
-
-}
+document.addEventListener('DOMNodeInserted', function() {
+  switch (true) {
+    case $.el('#container_story') == null:
+      // if #container_story is not yet loaded
+      break;
+  case $.el('#container_story') != null:
+      //hack vertical scroll to horizontal scroll
+      let containerInvertScroll = $.el("#container_story");
+      //Webkit
+      containerInvertScroll.addEventListener('mousewheel', () => new scrollHorizontal(window.event, containerInvertScroll))
+      //Gecko
+      containerInvertScroll.addEventListener('DOMMouseScroll', () => new scrollHorizontal(window.event, containerInvertScroll))
+      break;
+  }
+});
